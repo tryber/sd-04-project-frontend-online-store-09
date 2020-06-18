736f20d6019aa155d203fd5a09319acf8c353796
igorgbr <igorgphotoarte@gmail.com>
2020-06-18 18:01:44 -0300

    cc

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { getProduct } from '../services/api';
 
-class ProductDetails extends Component {
+class ProductDetails extends React.Component {
+  async componentDidMount() {
+    const data = await getProduct(this.props.match.params.productId);
+    this.handleProduct(data);
+  }
   render() {
-    return <div></div>;
+    return;
   }
 }
 
