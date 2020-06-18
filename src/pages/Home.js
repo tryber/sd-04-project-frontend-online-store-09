c7edeb5e13089a46f382e6bfb329b92e48610d46
igorgbr <igorgphotoarte@gmail.com>
2020-06-18 10:49:21 -0300

    Botao do carrinho de compras criado

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import SearchBar from '../components/SearchBar';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return (
       <div>
         <SearchBar />
+        <Link data-testid="shopping-cart-button" to="/ShoppingCart">
+          Carrinho de compras
+        </Link>
       </div>
     );
   }
