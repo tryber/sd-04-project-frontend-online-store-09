4272a4e6b0aa9d5b63aabc9a41cd3b1ec6ba5a55
juniomelos <juniomelos@gmail.com>
2020-06-18 12:36:21 -0300

    Refiz a requisiçao da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,13 @@
 export async function getCategories() {
   const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
-  const categories = await response.json();
-  return categories;
+  const data = await response.json();
+  return data;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const urlCategoryId = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
-  const urlQuery = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
-  const urlCategoryQuery = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
-  let url;
-  if (categoryId) url = urlCategoryId;
-  else if (query) url = urlQuery;
-  else url = urlCategoryQuery;
-  const response = await fetch(url);
-  const product = await response.json();
-  return product;
+  const response = await fetch(
+    `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`,
+  );
+  const data = await response.json();
+  return data;
 }
