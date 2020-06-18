78b72d3aeb73e504838b4d7936150aecfdd10809
Lourenço <loulage@gmail.com>
2020-06-18 17:41:22 -0300

    Adicionado CategoryPanel, no qual lista as categorias dos produtos. Rederizado em Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import SearchBar from '../components/SearchBar';
+import CategoryPanel from '../components/CategoryPanel';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { selectedCategory: '' };
+    this.setCategory = this.setCategory.bind(this);
+  }
+
+  setCategory(category) {
+    this.setState({ selectedCategory: category });
+  }
+
   render() {
     return (
       <div>
         <SearchBar />
+        <CategoryPanel setCategory={this.setCategory} />
         <Link data-testid="shopping-cart-button" to="/shoppingcart">
           Carrinho de compras
         </Link>
