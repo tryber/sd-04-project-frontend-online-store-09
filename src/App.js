4f577a14ca7057b28c1bbad3cee813ac1975cc91
igorgbr <igorgphotoarte@gmail.com>
2020-06-18 17:48:12 -0300

    excluido productDetail.jsx - incluido rota

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import ShoppingCart from './pages/ShoppingCart';
 import './App.css';
+import ProductDetails from './pages/ProductDetails';
 
 class App extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/shoppingcart" component={ShoppingCart} />
+          <Route path="/product/:pID" component={ProductDetails} />
         </Switch>
       </BrowserRouter>
     );
---
+++ src/pages/ProductDetails.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { getProduct } from '../services/api';
+
+class ProductDetails extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default ProductDetails;
