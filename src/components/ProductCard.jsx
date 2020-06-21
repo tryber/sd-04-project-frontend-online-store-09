87188fd4db111e8434b9c0d70cf0ad240a12cf48
igorgbr <igorgphotoarte@gmail.com>
2020-06-20 22:52:35 -0300

    rota do carrinho de compras corrigida

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -14,14 +14,17 @@ export class ProductCard extends Component {
           <h4 data-testid="product-detail-name">{title}</h4>
           <h5>{id}</h5>
           <h5>{`R$ ${price}`}</h5>
-          <Link data-testid="product-detail-link" to={{ pathname: `${id}/details/`, state: { product } }}>Detalhes</Link>
-          <button
-            className="product-card-btn"
-            type="button"
-            data-testid="product-add-to-cart"
+          <Link
+            data-testid="product-detail-link"
+            to={{ pathname: `${id}/details/`, state: { product } }}
           >
-            Adicionar ao Carrinho
-          </button>
+            Detalhes
+          </Link>
+          <Link data-testid="shopping-cart-button" to="/shoppingcart">
+            <button className="product-card-btn" type="button" data-testid="product-add-to-cart">
+              Adicionar ao Carrinho
+            </button>
+          </Link>
         </div>
       </div>
     );
